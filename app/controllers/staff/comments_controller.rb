e04ceb0b8a4d26bbf2c43d98ba2a23d343ddedfc
Arne Brasseur <arne@arnebrasseur.net>
2019-03-19 15:32:36 +0100

    comments in reverse chronological order

--- app/controllers/staff/comments_controller.rb
+++ app/controllers/staff/comments_controller.rb
@@ -3,6 +3,6 @@ class Staff::CommentsController < Staff::ApplicationController
   before_action :enable_staff_event_subnav
 
   def index
-    @comments = Comment.order(:created_at).select { |c| c.proposal.event_id == @event.id }
+    @comments = Comment.order(created_at: :desc).select { |c| c.proposal.event_id == @event.id }
   end
 end
